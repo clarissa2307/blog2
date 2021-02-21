bbec39ae6ef316d31af5601ea0ba9f39d221fee3
clarissa2307 <marthaleo07@gmail.com>
2021-02-20 20:54:50 -0600

    editar

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Crear artículo</div>
+                <div class="card-header">Editar artículo</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,29 +15,36 @@
                     @endif
 
                     <form
-                        action="{{ route('posts.store') }}"
+                        action="{{ route('posts.update', $post) }}"
                         method="POST"
-                        enctype="multipart/form-data"                        
+                        enctype="multipart/form-data"
                     >
                     <div class="form-group">
                         <label>Título *</label>
-                        <input type="text" name="title" class="form-control" required>
+                        <input
+                            type="text"
+                            name="title"
+                            class="form-control"
+                            required
+                            value="{{ old('title', $post->title) }}">
+                        </input>
                     </div>
                     <div class="form-group">
-                        <label>Image</label>
+                        <label>Imagen</label>
                         <input type="file" name="file">
                     </div>
                     <div class="form-group">
                         <label>Contenido *</label>
-                        <textarea name="body" rows="6" class="form-control" required></textarea>
+                        <textarea name="body" rows="6" class="form-control" required>{{ old('body', $post->body) }}</textarea>
                     </div>
                     <div class="form-group">
                         <label>Contenido embebido</label>
-                        <textarea name="iframe" class="form-control"></textarea>
+                        <textarea name="iframe" class="form-control">{{ old('iframe', $post->iframe) }}</textarea>
                     </div>
                     <div class="form-group">
                         @csrf
-                        <input type="submit" value="Enviar" class="btn btn-sm btn-primary">
+                        @method('PUT')
+                        <input type="submit" value="Actualizar" class="btn btn-sm btn-primary">
                     </div>
                     </form>
                 </div>
